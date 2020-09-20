8e78bfe57ca213c67cb9890b2e8aa6ed701f9bae
Francislainy Campos <francislainy.campos@hmhco.com>
2020-09-20 09:37:02 +0100

    add swagger documentation

---
+++ src/main/java/com/francislainy/gatling_tool/util/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package com.francislainy.gatling_tool.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * https://stark-bastion-15836.herokuapp.com/swagger-ui.html#/
+ * http://localhost:8080/swagger-ui.html#/product-controller
+ */
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
