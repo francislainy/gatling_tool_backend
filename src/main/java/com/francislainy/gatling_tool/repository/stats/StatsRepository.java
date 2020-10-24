c50c2e3d0b83f9b0c6876ca3226e76cb2445e5c1
Francislainy Campos <francislainy.campos@hmhco.com>
2020-10-25 00:44:00 +0100

    GT-26 Order stats by id on api #GT-26 Fixed

--- src/main/java/com/francislainy/gatling_tool/repository/stats/StatsRepository.java
+++ src/main/java/com/francislainy/gatling_tool/repository/stats/StatsRepository.java
@@ -1,6 +1,7 @@
 package com.francislainy.gatling_tool.repository.stats;
 
 import com.francislainy.gatling_tool.model.entity.stats.StatsEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,6 +9,9 @@ import java.util.UUID;
 
 public interface StatsRepository extends CrudRepository<StatsEntity, UUID> {
 
+    String queryStatsOrderedById = "SELECT * FROM stats WHERE report.id = :id ORDER BY id";
+
+    @Query(value = queryStatsOrderedById, nativeQuery = true)
     List<StatsEntity> findByReportId(UUID reportId);
 
 }
