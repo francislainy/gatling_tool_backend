199c9764abbb65286d18797adfcd6cd0de2b21a6
Francislainy Campos <francislainy.campos@hmhco.com>
2020-11-13 01:31:51 +0000

    Remove unused pieces from api class

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,56 +1,9 @@
 const axios = require('axios')
-const Category = require("../model/Category");
-
-const url = "http://localhost:8081"
 
 class Api {
 
     constructor(url) {
         this.url = url
-    }
-
-    retrieveTable() {
-        return axios.get('http://localhost:8081/api/gatling-tool/report')
-    }
-
-    deleteReport(id) {
-        return axios.delete(`http://localhost:8081/api/gatling-tool/report/${id}`)
-    }
-
-    retrieveReportItem(id) {
-        return axios.get(`http://localhost:8081/api/gatling-tool/report/${id}`)
-    }
-
-    updateReport(id, payload) {
-        return axios.put(`http://localhost:8081/api/gatling-tool/report/${id}`, payload)
-    }
-
-    createReport(payload) {
-        return axios.post('http://localhost:8081/api/gatling-tool/report', payload)
-    }
-
-    retrieveStatsForReport(id) {
-        return axios.get(`http://localhost:8081/api/gatling-tool/stats/report/${id}`)
-    }
-
-    deleteStats(id) {
-        return axios.delete(`http://localhost:8081/api/gatling-tool/stats/${id}`)
-    }
-
-    updateStatsEndpoint(id, payload) {
-        return axios.put(`http://localhost:8081/api/gatling-tool/stats/${id}/endpoint`, payload)
-    }
-
-    retrieveCategories() {
-        return axios.get('http://localhost:8081/api/gatling-tool/category')
-    }
-
-    createCategory(payload) {
-        return axios.post('http://localhost:8081/api/gatling-tool/category', payload)
-    }
-
-    retrieveCategory(id) {
-        return axios.get(`${this.url}/api/gatling-tool/category/${id}`).then(r => new Category(r.data.id, r.data.name));
     }
 
     submitFile(file) {
